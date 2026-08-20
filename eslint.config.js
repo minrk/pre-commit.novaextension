4706a60019771e45b6d713eaa9483f579b52d47f
Min RK <benjaminrk@gmail.com>
2026-08-20 12:03:31 -0700

    use language: unsupported
    this is wrong, but don't bother interacting with pre-commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,36 @@
-const {
-    defineConfig,
-} = require("eslint/config");
+const { defineConfig } = require("eslint/config");
 
 const globals = require("globals");
 const nova = require("eslint-plugin-nova");
 const js = require("@eslint/js");
 
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
+const { FlatCompat } = require("@eslint/eslintrc");
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-module.exports = defineConfig([{
+module.exports = defineConfig([
+  {
     languageOptions: {
-        globals: {
-            ...globals.node,
-            ...nova.environments.nova.globals,
-        },
+      globals: {
+        ...globals.node,
+        ...nova.environments.nova.globals,
+      },
 
-        ecmaVersion: 2018,
-        sourceType: "module",
-        parserOptions: {},
+      ecmaVersion: 2018,
+      sourceType: "module",
+      parserOptions: {},
     },
 
     extends: compat.extends("eslint:recommended"),
 
     plugins: {
-        nova,
+      nova,
     },
 
     rules: {},
-}]);
+  },
+]);
