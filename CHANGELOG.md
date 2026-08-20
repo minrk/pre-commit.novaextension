893110cf3329aa98291d0ffa9b651dc428f308b9
Min RK <benjaminrk@gmail.com>
2026-08-20 13:16:30 -0700

    1.1: support and prefer prek

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## Version 1.1.0
+
+Add support for `prek` (rust implementation, faster, more user-friendly),
+which is preferred by default, and allow overriding executable via config.
+
 ## Version 1.0.1
 
 Use relative paths, fixing some configuration detection, e.g. `.prettierignore`
--- Scripts/main.js
+++ Scripts/main.js
@@ -1,14 +1,27 @@
+const disposables = [];
+
 exports.activate = function () {
   // Do work when the extension is activated
   // TODO: should we move this event watcher
-  nova.workspace.onDidAddTextEditor((editor) => {
-    editor.onDidSave(preCommitOnSave);
-  });
+  disposables.push(
+    nova.workspace.onDidAddTextEditor((editor) => {
+      editor.onDidSave(preCommitOnSave);
+    }),
+  );
+  disposables.push(
+    nova.config.onDidChange("pre-commit.preferPreK", () => {
+      // changing prefer prek resets search
+      foundPreCommit = null;
+    }),
+  );
 };
 
 exports.deactivate = function () {
   // Clean up state before the extension is deactivated
-  // how do I unregister onDidAddTextEditor
+  while (disposables.length) {
+    const d = disposables.pop();
+    d.dispose();
+  }
 };
 
 const preCommitCache = {};
@@ -42,9 +55,51 @@ function preCommitOnSave(editor) {
   preCommit(["--files", nova.path.basename(path)], nova.path.dirname(path));
 }
 
-function preCommit(args, cwd, showOnError = false) {
+let foundPreCommit = null;
+
+async function preCommitExecutable() {
+  let exe = nova.config.get("pre-commit.executable");
+  if (exe) {
+    return exe;
+  }
+  if (foundPreCommit) {
+    return foundPreCommit;
+  }
+  let preferPreK = nova.config.get("pre-commit.preferPreK");
+  let searchList = ["prek", "pre-commit"];
+  if (!preferPreK) {
+    searchList = ["pre-commit", "prek"];
+  }
+  for (exe of searchList) {
+    console.log("looking for", exe);
+    try {
+      let result = await new Promise((resolve) => {
+        const process = new Process("/usr/bin/env", { args: [exe, "-h"] });
+        process.onDidExit(resolve);
+        process.start();
+      });
+      if (result === 0) {
+        console.log(`using executable: ${exe}`);
+        foundPreCommit = exe;
+        return exe;
+      } else {
+        console.log(`${exe} not found`);
+      }
+    } catch (e) {
+      console.log(`error looking for ${exe}`, e);
+    }
+  }
+  nova.workspace.showWarningMessage("Neither pre-commit nor prek found.");
+  return null;
+}
+
+async function preCommit(args, cwd, showOnError = false) {
+  const exe = await preCommitExecutable();
+  if (!exe) {
+    return;
+  }
   var options = {
-    args: ["pre-commit", "run"].concat(args),
+    args: [exe, "run"].concat(args),
     cwd: cwd,
   };
   console.log(options.args.join(" "));
